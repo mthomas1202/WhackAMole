866b3af3a1fd11656df62a62117448f541952b81
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-08-28 00:08:12 -0400

    game logic and interface design

---
+++ Assets/Mole.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+
+    public float visibleHeight = 0.2f;
+    public float hiddenHeight = -0.3f;
+    public float speed = 4f;
+    public float disappearDuration = 0.5f;
+
+    private float disappearTimer = 0f;
+    private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Awake () {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z);
+
+        transform.localPosition = targetPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+        disappearTimer -= Time.deltaTime;
+        if(disappearTimer < 0f)
+        {
+            Hide();
+        }
+	}
+
+    public void Rise()
+    {
+        targetPosition = new Vector3(
+           transform.localPosition.x,
+           visibleHeight,
+           transform.localPosition.z);
+
+        disappearTimer = disappearDuration;
+    }
+
+    public void Hide()
+    {
+        targetPosition = new Vector3(
+           transform.localPosition.x,
+           hiddenHeight,
+           transform.localPosition.z);
+    }
+    public void OnHit()
+    {
+        Hide();
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward,out hit) || Input.GetKeyDown("space"))
+        {
+            if (hit.transform.GetComponent<Mole>() != null)
+            {
+                Mole mole = hit.transform.GetComponent<Mole>();
+                mole.OnHit();
+
+                score++;
+            }
+        }
+	}
+}
